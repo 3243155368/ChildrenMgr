9d1af83a906330b1e6bb76df76cfe1957d3c27c3
zhujiaxing <3243155368@qq.com>
2025-10-19 19:10:47 +0800

    缓存服内容补充，数据存储

--- Packages/cn.etetet.unit/Scripts/Hotfix/Share/UnitSystem.cs
+++ Packages/cn.etetet.unit/Scripts/Hotfix/Share/UnitSystem.cs
@@ -4,6 +4,15 @@ namespace ET
     public static partial class UnitSystem
     {
         [EntitySystem]
+        private static void GetComponentSys(this ET.Unit self, System.Type args2)
+        {
+            if (!typeof(IUnitCache).IsAssignableFrom(args2))
+            {
+                return;
+            }
+            EventSystem.Instance.Publish(self.Scene(),new UnitGetComponent(){Unit = self, Type = args2});
+        }
+        [EntitySystem]
         private static void Awake(this Unit self, int configId)
         {
             self.ConfigId = configId;
